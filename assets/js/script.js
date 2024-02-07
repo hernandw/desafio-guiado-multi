bed1d337d59728ca6f65b107703cc6a24e36e91e
Williams Hernández <hernandw@gmail.com>
2024-02-07 16:23:33 -0300

    :sparkles: factorial

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-let num = 3;
+let num = +prompt('Ingresa un numero entre 1 y 20')
 
 //funcion tradicional
 /* function mostrarDatos (num) {
@@ -12,17 +12,25 @@ let num = 3;
 //Function tipo arrow function
 let mostrarDatos = (num) => {
   if (num < 1 || num > 20) {
-    console.log("número fuera del rango");
+      console.log("número fuera del rango")
+      return
+  }
+
+  //Tabla de multiplicacion
+
+  for (let i = 1; i <= num; i++) {
+    console.log(`${i} x ${num} = ${i * num}`);
+  }
+
+  //Factorial
+
+  for (let x = 1; x <= num; x++) {
+    let res = 1; // se inicializa la variable
+    for (let j = 1; j <= x; j++) {
+      res = res * j; //calcula el resulta del factorial
     }
-
-    //Tabla de multiplicacion
-
-    for (let i = 1; i <= num; i++){
-        let resultado = i * num
-        console.log(`${i} x ${num} = ${resultado}`)
-    } 
-    
-
+    console.log(`Factorial de ${x}  es:  ${res}`); //muestra el valor del factorial
+  }
 };
 
 mostrarDatos(num);
